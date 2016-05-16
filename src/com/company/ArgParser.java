ed57380528b7e4bbeab9cae9040581826be688b6
madewithlinux <madewithlinux.josh@gmail.com>
2016-05-15 20:57:07 -0500

    added whitelist and option to not print the header

---
+++ src/com/company/ArgParser.java
@@ -0,0 +1,104 @@
+package com.company;
+
+import sun.rmi.server.InactiveGroupException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ArgParser {
+    private enum ArgumentType {
+        KEY_VALUE,
+        IF_PRESENT,
+    }
+
+    private HashMap<String, ArgumentType> argTypes;
+    private HashMap<Integer, String> allArgs;
+    private HashMap<String, Integer> flagToKey;
+    private ArrayList<String> miscArgs;
+
+    public ArgParser() {
+        this.argTypes = new HashMap<>();
+        this.allArgs = new HashMap<>();
+        this.flagToKey = new HashMap<>();
+        this.miscArgs = new ArrayList<>();
+    }
+
+    public ArgParser putKeyValue(String flag, Integer key) {
+        argTypes.put(flag, ArgumentType.KEY_VALUE);
+        flagToKey.put(flag, key);
+        return this;
+    }
+
+    public ArgParser putDefault(Integer key, String defaultValue) {
+        allArgs.put(key, defaultValue);
+        return this;
+    }
+
+    public ArgParser putIfPresent(String flag, Integer key) {
+        argTypes.put(flag, ArgumentType.IF_PRESENT);
+        flagToKey.put(flag, key);
+        return this;
+    }
+
+    public ArgParser parse(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (!args[i].startsWith("-")) {
+                miscArgs.add(args[i]);
+                continue;
+            }
+            /*todo: -flag=value argument types*/
+            /*todo: detect double-dashes, e.g. --flag*/
+            switch (argTypes.get(args[i])) {
+                case KEY_VALUE:
+                    allArgs.put(flagToKey.get(args[i]), args[i + 1]);
+                    i++; /*advance i because the next one is a non-argument*/
+                    break;
+                case IF_PRESENT:
+                    /*empty non-null value*/
+                    allArgs.put(flagToKey.get(args[i]), "");
+                    break;
+            }
+        }
+        return this;
+    }
+
+    public String getValue(Integer key) {
+        return allArgs.get(key);
+    }
+
+    public ArrayList<String> getMiscArgs() {
+        return miscArgs;
+    }
+
+    public boolean isPresent(Integer key) {
+        return allArgs.get(key) != null;
+    }
+
+
+    /* test case*/
+    public static void main(String[] args) {
+        String[] testCase = "-a 4 -v".split(" ");
+        int key_a = 1;
+        int key_b = 2;
+        int key_v = 3;
+        int key_d = 4;
+        ArgParser parser = new ArgParser()
+                .putIfPresent("-v", key_v)
+                .putKeyValue("-a", key_a)
+                .putKeyValue("-b", key_b)
+                .putKeyValue("-d", key_d)
+                .putDefault(key_d, "5")
+                .parse(testCase);
+        test(parser.getValue(key_a).equals("4"), "key value");
+        test(parser.isPresent(key_v), "is present v");
+        test(!parser.isPresent(key_b), "is present b");
+        test(parser.getValue(key_d).equals("5"), "key value");
+    }
+
+    private static void test(boolean expr, String message) {
+        if (!expr) {
+            System.out.println("Test failed: " + message);
+        }
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,65 +6,86 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-
     private static String hostnamePath = "/etc/hostname";
     private static String headder = "# hosts file made by j0sh\n" +
             "127.0.0.1 %s # current hostname for this machine\n" +
             "::1 %s # current hostname for this machine\n" +
             "127.0.0.1 localhost # IPv4 localhost\n" +
             "::1 localhost #IPv6 localhost\n";
+    private static int KEY_NO_HEADDER = 1;
+    private static int KEY_WHITELIST_PATH = 2;
 
     public static void main(String[] args) {
-
-        if (args.length < 1) {
+        ArgParser arglist = new ArgParser()
+                .putIfPresent("-nh", KEY_NO_HEADDER)
+                .putKeyValue("-w", KEY_WHITELIST_PATH)
+                .parse(args);
+        if (arglist.getMiscArgs().size() < 1) {
             System.out.println("Usage: java -jar <this .jar file> <path to URL file>");
             System.out.println("URL file should be a file with one remote url per line");
+            System.out.println("Optional: ");
+            System.out.println(" -nh           do not print the header");
+            System.out.println(" -w <file>     ignore the hosts specified in file");
             return;
         }
 
-        String hostUrlsFilePath = args[0];
-        ArrayList<String> hostUrls = new ArrayList<>();
-        try {
-            hostUrls = new BufferedReader(new FileReader(hostUrlsFilePath))
-                    .lines()
-                    /*Allow C and Python style comments*/
-                    .filter(s -> (!s.startsWith("#") && !s.startsWith("//")))
-                    .filter(s -> (!s.isEmpty()))
-                    .collect(Collectors.toCollection(ArrayList::new));
-        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-            System.err.println("Could not read input file");
-            return;
+        /*read the whitelist file if we specify one*/
+        final Set<String> whitelist = new HashSet<>();
+        if (arglist.isPresent(KEY_WHITELIST_PATH)) {
+            /*addAll instead of assignment so that the variable may be final*/
+            readFileByLines(arglist.getValue(KEY_WHITELIST_PATH))
+                    .stream()
+                    .forEach(whitelist::add);
         }
 
         /*print the header and hostname*/
-        String hostname = "localhost";
-        try {
-            hostname = new BufferedReader(new FileReader(hostnamePath)).readLine();
-        } catch (IOException e) {
-            System.err.println("Could not read hostname, defaulting to localhost");
-            e.printStackTrace();
+        if (!arglist.isPresent(KEY_NO_HEADDER)) {
+            String hostname = "localhost";
+            try {
+                hostname = new BufferedReader(new FileReader(hostnamePath)).readLine();
+            } catch (IOException e) {
+                System.err.println("Could not read hostname, defaulting to localhost");
+                e.printStackTrace();
+            }
+            System.out.println(String.format(headder, hostname, hostname));
         }
-        System.out.println(String.format(headder, hostname, hostname));
 
         /*download and build the host list and then format and print it*/
-        StringBuilder builder = new StringBuilder();
-        hostUrls.parallelStream()
+        Set<String> hostUrls = readFileByLines(arglist.getMiscArgs().get(0));
+        System.out.println(hostUrls.parallelStream()
                 .map(Main::downloadToSet)
                 .reduce((strings, strings2) -> {
                     strings.addAll(strings2);
                     return strings;
                 })
                 .get()
-                .forEach(builder::append);
-        System.out.println(builder.toString());
+                .parallelStream()
+                .filter(s -> !whitelist.contains(s))
+                .collect(Collectors.joining("\n"))
+        );
+    }
+
+    private static boolean isNotComment(String s) {
+        /*Allow C and Python style comments*/
+        return !s.startsWith("#") && !s.startsWith("//") && !s.isEmpty();
+    }
+
+    private static Set<String> readFileByLines(String path) {
+        try {
+            return new BufferedReader(new FileReader(path))
+                    .lines()
+                    .filter(Main::isNotComment)
+                    .collect(Collectors.toCollection(HashSet::new));
+        } catch (FileNotFoundException e) {
+            System.err.println("Could not read input file");
+            return new HashSet<>();
+        }
     }
 
     private static Set<String> downloadToSet(String url) {
         BufferedInputStream in;
         try {
-            URL source = new URL(url);
-            in = new BufferedInputStream(source.openStream());
+            in = new BufferedInputStream(new URL(url).openStream());
         } catch (IOException e) {
             /*if we could not download, ignore*/
             System.err.println("Failed to download: " + url);
@@ -77,14 +98,12 @@ public class Main {
         }
         return lines.parallelStream()
                 /*Allow C and Python style comments*/
-                .filter(s -> (!s.startsWith("#") && !s.startsWith("//")))
-                .filter(s -> (!s.isEmpty()))
+                .filter(Main::isNotComment)
                 .map(line -> "0.0.0.0 " + line
                         .replace("127.0.0.1", "")
                         .replace("0.0.0.0", "")
                         .replace("\t", "")
-                        .replace(" ", "")
-                        + "\n")
+                        .replace(" ", ""))
                 .collect(Collectors.toSet());
     }
 }
